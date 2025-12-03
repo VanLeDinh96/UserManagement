672c0953d215ba3c4f080108d62d5c84c8ebabde
VanLeDinh96 <dinhvanle.it@gmail.com>
2025-12-03 23:10:40 +0700

    add form create user

--- UserManagement/CreateUser.Designer.cs
+++ UserManagement/CreateUser.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CreateUser";
+            lblHeaderCreateUser = new Label();
+            lblUsername = new Label();
+            txtUsername = new TextBox();
+            lblPassword = new Label();
+            txtPassword = new TextBox();
+            btnCreate = new Button();
+            SuspendLayout();
+            // 
+            // lblHeaderCreateUser
+            // 
+            lblHeaderCreateUser.AutoSize = true;
+            lblHeaderCreateUser.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblHeaderCreateUser.Location = new Point(203, 9);
+            lblHeaderCreateUser.Name = "lblHeaderCreateUser";
+            lblHeaderCreateUser.Size = new Size(154, 37);
+            lblHeaderCreateUser.TabIndex = 0;
+            lblHeaderCreateUser.Text = "Create User";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(14, 93);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(60, 15);
+            lblUsername.TabIndex = 1;
+            lblUsername.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(114, 90);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(407, 23);
+            txtUsername.TabIndex = 2;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(14, 142);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(57, 15);
+            lblPassword.TabIndex = 3;
+            lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(114, 139);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(407, 23);
+            txtPassword.TabIndex = 4;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnCreate
+            // 
+            btnCreate.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCreate.Location = new Point(228, 216);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(88, 31);
+            btnCreate.TabIndex = 5;
+            btnCreate.Text = "Create";
+            btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // CreateUser
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(542, 278);
+            Controls.Add(btnCreate);
+            Controls.Add(txtPassword);
+            Controls.Add(lblPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblUsername);
+            Controls.Add(lblHeaderCreateUser);
+            Name = "CreateUser";
+            Text = "Create User";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblHeaderCreateUser;
+        private Label lblUsername;
+        private TextBox txtUsername;
+        private Label lblPassword;
+        private TextBox txtPassword;
+        private Button btnCreate;
     }
 }
